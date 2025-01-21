1c0eb99522bd7f9bb813ae061b473dc2d6b954f8
MahnoorAmin456 <mahnooramin28@gmail.com>
2025-01-22 00:46:05 +0500

    changes code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image from "next/image" ;
 import Link from "next/link";
 import nikeBanner from "@public/images/nike-banner.jpg";
 import featureBanner from "@public/images/featured-banner.jpg";
